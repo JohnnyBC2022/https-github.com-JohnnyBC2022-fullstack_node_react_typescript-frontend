183da84964358b74aab3cff781b830cc3a79b1c7
Johnny <jonathanbc2012@gmail.com>
2025-01-24 17:19:02 +0100

    feat: router created with layouts and views components

---
+++ client/src/router.tsx
@@ -0,0 +1,22 @@
+import {createBrowserRouter} from 'react-router-dom'
+
+import Products from './views/Products'
+import NewProduct from './views/NewProduct'
+import Layout from './layouts/Layout'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                index: true,
+                element: <Products />
+            },
+            {
+                path: 'productos/nuevo',
+                element: <NewProduct />
+            }
+        ]
+    }
+])
