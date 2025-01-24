82a58c0a7b5909dc359306b6eca760842d789d47
Johnny <jonathanbc2012@gmail.com>
2025-01-24 17:44:16 +0100

    feat: NewProduct and Product modified. Styles modified

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -3,8 +3,17 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <div>Layout</div>
-      <Outlet />
+      <header className="bg-teal-900 ">
+        <div className="mx-auto max-w-6xl py-10">
+          <h1 className="text-white text-4xl font-extrabold">
+            Administrador de Productos
+          </h1>
+        </div>
+      </header>
+
+      <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
+        <Outlet />
+      </main>
     </>
   );
 }
--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,7 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
-  )
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-700">Añadir Producto</h2>
+        <Link
+          to="/"
+          className="rounded-md bg-teal-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-teal-500"
+        >
+          Volver a Productos
+        </Link>
+      </div>
+    </>
+  );
 }
